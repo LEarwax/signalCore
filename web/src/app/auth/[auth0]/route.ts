0bd6a21e2ae8e0cf8b297aca90b0745208604cf1
Nick Dowl <nickdowl@gmail.com>
2026-07-06 15:02:49 -0700

    fix: use auth0.middleware in route handler

--- web/src/app/auth/[auth0]/route.ts
+++ web/src/app/auth/[auth0]/route.ts
@@ -2,9 +2,9 @@ import { auth0 } from "@/lib/auth0";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
-  return auth0.handler(request);
+  return auth0.middleware(request);
 }
 
 export async function POST(request: NextRequest) {
-  return auth0.handler(request);
+  return auth0.middleware(request);
 }
